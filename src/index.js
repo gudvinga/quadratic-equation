49e84d5eae9d0d128f974f2a720e533cbb5d7b9e
gudvinga <gudvinga@gmail.com>
2018-02-20 15:30:14 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  equation = equation.replace(/\s/g,'');
+  let arr = equation.split('x');
+
+  const A = parseInt(arr[0]),
+        B = parseInt(arr[1].slice(2)),
+        C = parseInt(arr[2]);
+
+  let D = Math.pow(B,2) - 4*A*C,
+      X1 = (-B - Math.sqrt(D)) / (2*A),
+      X2 = (-B + Math.sqrt(D)) / (2 * A);
+
+  return [X1, X2];
 }
